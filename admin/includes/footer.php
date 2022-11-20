458c2797477b9e76e8253e91b3524b2a6a8ebdc2
suleiman yahya garo <suleimanyahyagaro@gmail.com>
2022-11-20 14:27:30 +0100

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,4 @@
+
+                <footer class="footer text-right">
+                   2020 © Developed by Suleiman yahya garo | © Powered By Soft TEchnnologies
+                </footer>
---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white"> 2020 © Developed by Suleiman yahya garo | © Powered By Soft TEchnnologies</p>
+      </div>
+      <!-- /.container -->
+    </footer>
